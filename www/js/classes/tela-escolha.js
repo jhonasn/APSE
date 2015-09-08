2f4b731523fbaa71d80c6bd263a65bdab94c362d
Jhonas Nascimento <jhonasn@gmail.com>
2015-09-08 11:18:17 -0300

    implementado classe de telaescolha

--- www/js/classes/tela-escolha.js
+++ www/js/classes/tela-escolha.js
@@ -2,9 +2,13 @@ var TelaEscolha = function() {
 	var self = this
 
 	this.escolhas = new Array//array de tela-reproducao
-	
+
 	if(arguments.length > 0) {
-		if(Array.isArray(arguments[0]) && typeof arguments[0][0] === 'TelaReproducao') {
+		if(
+			Array.isArray(arguments[0]) && 
+			typeof arguments[0][0] === 'object' &&
+			arguments[0][0] instanceof TelaReproducao
+		) {
 			self.escolhas = arguments[0]
 		}
 	}
