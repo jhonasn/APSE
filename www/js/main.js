e4419d9c2d44acb621a35defa829bf5af18052e3
Jhonas Nascimento <jhonasn@gmail.com>
2015-09-08 23:08:04 -0300

    implementado chamar telas de reprodução e escolha

--- www/js/main.js
+++ www/js/main.js
@@ -1,77 +1,91 @@
 $(document).ready(function() {
 	var svgDoc = $('#main-btns').get(0)
-	var contentHtml = null
+	var content = null
+	var mensagemErro = 'Houve um erro ao acessar os dados base do aplicativo. Tente reiniciar o aplicativo.'
 
 	$(svgDoc).on('load', function() {
 		svgDoc = svgDoc.contentDocument
 
 		$(svgDoc).find('g[id].clicable').click(function() {
 			var selecionado = $(this).attr('id')
-			var html = $(contentHtml)
 
-			switch(selecionado) {
-				case 'btn-fogo':
-					selecionado = 'queimaduras'
-					break;
-				case 'btn-cabeca':
-					selecionado = 'epilepsia'
-					break;
-				case 'btn-mao-faca':
-					selecionado = 'cortes-hemorragia'
-					break;
-				case 'btn-cabeca-garganta':
-					selecionado = 'engasgo'
-					break;
-				case 'btn-pessoa-caindo':
-					selecionado = 'quedas'
-					break;
-				case 'btn-mao-agua':
-					selecionado = 'afogamento'
-					break;
-				case 'btn-coracao':
-					selecionado = 'parada-cardiaca'
-					break;
-				case 'btn-carro':
-					selecionado = 'acidentes-transito'
-					break;
-				/*
-				case 'btn-logo':
-					break;
-				*/
-			       default:
-				       selecionado = null
-				       break;
+			var telaContent = content.telas.filter(function(t) {
+				return t.icone === selecionado
+			})
 
+			if(telaContent.length > 0) {
+				telaContent = telaContent[0]
+			} else {
+				return
 			}
 
-			if(selecionado !== null) {
-				var content = $(html).find('#' + selecionado)
-				console.log(content)
+			if(telaContent.hasOwnProperty('escolhas') && Array.isArray(telaContent.escolhas)) {
+				console.log('tela escolha preencher com:', telaContent)
+			} else {
+				console.log('tela reproducao preencher com:', telaContent)
 			}
 		})
 	})
 
 	$.get('data/conteudo-telas.json')
 	.success(function(data) {
-		contentHtml = data
+		content = data
 		console.log('ok json pego:', data)
 	})
 	.error(function(err) {
-		alert('Houve um erro ao acessar os dados base do aplicativo. Tente reiniciar o aplicativo.')
+		alert(mensagemErro)
 	})
 
+	var abrirTelaReproducao = function(dadosTela) {
+		$.get('templates/tela-reproducao.html')
+		.success(function(content) {
+			content.replace('{titulo}', dadosTela.titulo)
+				.replace('{icone}', dadosTela.icone)
+			content = $(content)
+
+			var instrucoesTemplate = $(content).find('#instrucoes').clone()
+			dadosTela.instrucoes.forEach(function(i) {
+				var template = $(instrucoesTemplate).clone()
+			})
+			$('#content').html(content)
+		})
+		.error(function(err) {
+			alert(mensagemErro)
+		})
+	}
+
+	var abrirTelaEscolha = function(dadosTela) {
+		$.get('templates/tela-escolha.html')
+		.success(function(content) {
+			content.replace('{titulo}', dadosTela.titulo)
+				.replace('{icone}', dadosTela.icone)
+			content = $(content)
+
+			/*
+			var instrucoesTemplate = $(content).find('#instrucoes').clone()
+			dadosTela.instrucoes.forEach(function(i) {
+				var template = $(instrucoesTemplate).clone()
+			})
+			$('#content').html(content)
+		        */
+		})
+		.error(function(err) {
+			alert(mensagemErro)
+		})
+	}
+
 	/*
-	$('#reproduzir').click(function() {
-		var txt = $('#texto').val()
-		TTS.speak(
-			txt,
-			function() {
-				alert('terminou de reproduzir a bagaca')
-			},
-			function(reason) {
-				alert('Deu erro, segue: ', reason)
-			}
-		)
-	})
-        */
+	   $('#reproduzir').click(function() {
+	   var txt = $('#texto').val()
+	   TTS.speak(
+	   txt,
+	   function() {
+	   alert('terminou de reproduzir a bagaca')
+	   },
+	   function(reason) {
+	   alert('Deu erro, segue: ', reason)
+	   }
+	   )
+	   })
+	   */
 })
